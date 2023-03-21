950d8bd826dc90c9b82c99582fb88959476a839b
adrianJ <123407879+AdrianJimenezNieto@users.noreply.github.com>
2023-03-21 08:58:54 +0100

    Product detail template
    Template for showing the product to the costumers

--- suministros/product/models.py
+++ suministros/product/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, blank=False, default='Sin nombre')
-    categoria = models.CharField(max_length=50, blank=False, default='Sin nombre')
-    shortDescription = models.CharField(max_length=200, blank=False, default='Sin descripcion')
-    description = models.TextField(max_length=1200, blank=False, default='Sin descripcion')
+    name = models.CharField(max_length=50, blank=False)
+    categoria = models.CharField(max_length=50, blank=False)
+    shortDescription = models.CharField(max_length=200, blank=False)
+    description = models.TextField(max_length=1200, blank=False)
     ean13 = models.CharField(max_length=13, blank=False)
     ref = models.CharField(max_length=13, blank=False)
     stock = models.IntegerField(blank=False)
