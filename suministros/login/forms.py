adb9f38968446f701a4869cf7f5e685182a83859
adrianJ <123407879+AdrianJimenezNieto@users.noreply.github.com>
2023-03-21 18:44:58 +0100

    template login

--- suministros/login/forms.py
+++ suministros/login/forms.py
@@ -4,4 +4,4 @@ class LoginForm(forms.Form):
 
     username = forms.CharField(max_length=50, required=True)
     password = forms.CharField(max_length=50,required=True)
-    accessLevel = forms.IntegerField(min_value=1,max_value=3, required=True)
+    accessLevel = forms.SelectMultiple(choices=['Cliente', 'Proveedor', 'Administrador'])
